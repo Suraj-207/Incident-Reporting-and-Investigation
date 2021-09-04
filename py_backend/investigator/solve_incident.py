65d472a9e53d4982e16c4dfc68a5740fb7e5d9db
Shubham Gantayat <sgantayat9@gmail.com>
2021-09-04 17:38:15 +0530

    investigator added

--- py_backend/investigator/solve_incident.py
+++ py_backend/investigator/solve_incident.py
@@ -17,7 +17,7 @@ class Incident:
             config.logger.log("ERROR", str(e))
             return {"status": False, "message": "Internal Server Error"}
 
-    def display_incident(self, _id):
+    def display_incident_by_id(self, _id):
         try:
             return config.mongo_db.my_db['incident'].find({
                 "_id": _id
@@ -26,23 +26,11 @@ class Incident:
             config.logger.log("ERROR", str(e))
             return {"status": False, "message": "Internal Server Error"}
 
-    def edit_report(self, _id, report):
+    def edit_incident(self, _id, report, evidence):
         try:
             new_val = {"_id": _id}
-            condition = {"$set": {"report": report}}
+            condition = {"$set": {"report": report}, "$push": {"evidence": ImageConvert().convert_to_b64(evidence)}}
             return config.mongo_db.update("incident", new_val, condition)
         except Exception as e:
             config.logger.log("ERROR", str(e))
             return {"status": False, "message": "Internal Server Error"}
-
-    def add_evidence(self, _id, image):
-        try:
-            new_val = {"_id": _id}
-            condition = {"$push": {"evidence": ImageConvert().convert_to_b64(image)}}
-            return config.mongo_db.update("incident", new_val, condition)
-        except Exception as e:
-            config.logger.log("ERROR", str(e))
-            return {"status": False, "message": "Internal Server Error"}
-
-
-
